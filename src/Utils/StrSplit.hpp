d2296d1b1e1da83bb0303821e886bd4c8f915d3e
mrnickzh <1234dxc@gmail.com>
2026-04-02 01:41:27 +0300

    linux fix

--- src/Utils/StrSplit.hpp
+++ src/Utils/StrSplit.hpp
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <vector>
-#include <ranges>
 #include <string>
 
 class StrSplit {
 public:
-    static std::vector<std::string> str_split(const std::string& str, std::string delimiter) {
+    static std::vector<std::string> str_split(std::string str, std::string delimiter) {
         std::vector<std::string> tokens;
-        for (const auto& view : std::views::split(str, delimiter)) {
-            tokens.push_back(std::string(view.begin(), view.end()));
+        size_t pos = 0;
+        std::string token;
+        while ((pos = str.find(delimiter)) != std::string::npos) {
+            token = str.substr(0, pos);
+            tokens.push_back(token);
+            str.erase(0, pos + delimiter.length());
         }
+        tokens.push_back(str);
+
         return tokens;
     }
 };
